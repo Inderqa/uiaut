15a3e0bee3636f39a086d7c5292012634ebf9d9a
Inderqa <inder093@gmail.com>
2023-10-17 18:08:58 +0530

    added headless for chrome

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ from playwright.sync_api import sync_playwright, Page
 @pytest.fixture(scope="function", autouse=True)
 def browser_fixture():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context(permissions=["clipboard-read", "clipboard-write"])
         page = context.new_page()
         yield browser, context, page
