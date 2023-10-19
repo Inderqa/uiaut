ab7f3be4c237586854165b5730550136378c4afc
Inderqa <79741516+Inderqa@users.noreply.github.com>
2023-10-19 14:55:40 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,11 +4,19 @@ import json
 import re
 from playwright.sync_api import sync_playwright
 
-@pytest.fixture(scope="function", autouse=True)
-def browser_fixture():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        context = browser.new_context(permissions=["clipboard-read", "clipboard-write"])
-        page = context.new_page()
-        yield browser, context, page
-        browser.close()
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.goto("http://playwright.dev")
+    print(page.title())
+    browser.close()
+# from playwright.sync_api import sync_playwright
+
+# @pytest.fixture(scope="function", autouse=True)
+# def browser_fixture():
+#     with sync_playwright() as p:
+#         browser = p.chromium.launch(headless=True)
+#         context = browser.new_context(permissions=["clipboard-read", "clipboard-write"])
+#         page = context.new_page()
+#         yield browser, context, page
+#         browser.close()
